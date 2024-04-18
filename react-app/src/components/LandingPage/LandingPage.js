8d10403813e0cc77dcd5d8f775791a5d526d85f0
will <woburns191919>
2024-04-18 11:16:14 -0500

    landing styles

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -1,31 +1,30 @@
-import "./landingPageStyles.css"
+import "./landingPageStyles.css";
 
 const LandingPage = () => {
   return (
     <main className="landing-page-wrapper">
       <div className="aib-description-wrapper">
         <article className="aib-description">
-          <h4>
-            Adventures in Breath
-          </h4>
-          <h2>
-            EXPLORE THE LAND, AIR, AND SEA
-          </h2>
-          <p>
-            other text
-          </p>
-
+          <h4>ADVENTURES IN BREATH</h4>
+          <h2>EXPLORE THE LAND, AIR, AND SEA</h2>
+          <p>other text</p>
         </article>
-
       </div>
 
       <div className="map-wrapper">
         <img src="../MapPlaceholder.png"></img>
-
       </div>
+      <div className="aib-description-wrapper">
+      <article className="aib-description">
+      <h4>ABOUT US</h4>
+      <h2>ADVENTURES IN BREATH TEAM MEMBERS</h2>
 
+      </article>
+      </div>
+      <footer>
+      </footer>
     </main>
-  )
-}
+  );
+};
 
 export default LandingPage;
